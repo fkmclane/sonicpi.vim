41cff90615436f6496e9a572d7ae964af81f83e2
Lily Foster <lily@lily.flowers>
2021-06-23 09:39:26 -0400

    make completion update script work on most linux distros in addition to macos

--- util/update_completion.sh
+++ util/update_completion.sh
@@ -1,16 +1,32 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 if [ -n "$1" ]; then
 	sonic_pi_app_path="$1"
-elif [ -e '/Applications/Sonic Pi.app/Contents/Resources/app/server/native/ruby/bin/ruby' ]; then
+	if [ -n "$2" ]; then
+		ruby_exe="$2"
+	fi
+elif [ -d '/Applications/Sonic Pi.app/Contents/Resources/app' ]; then
 	sonic_pi_app_path='/Applications/Sonic Pi.app/Contents/Resources/app'
-elif [ -e '/opt/sonic-pi/app/server/native/ruby/bin/ruby' ]; then
+elif [ -d '/opt/sonic-pi/app' ]; then
 	sonic_pi_app_path='/opt/sonic-pi/app'
-elif [ -e '/usr/lib/sonic-pi/server/native/ruby/bin/ruby' ]; then
+elif [ -d '/usr/lib/sonic-pi/server' ]; then
 	sonic_pi_app_path='/usr/lib/sonic-pi'
+elif which sonic-pi >/dev/null 2>&1; then
+	sonic_pi_app_path="$(dirname "$(dirname "$(realpath "$(which sonic-pi)")")")"/app
 else
 	echo 'Could not find Sonic Pi' >&2
 	exit 1
 fi
 
-"$sonic_pi_app_path/server/native/ruby/bin/ruby" --enable-frozen-string-literal -E utf-8 "$(dirname "$0")"/update_completion.rb "$sonic_pi_app_path"
+if [ -z "$ruby_exe" ]; then
+	if [ -x "$sonic_pi_app_path/server/native/ruby/bin/ruby" ]; then
+		ruby_exe="$sonic_pi_app_path/server/native/ruby/bin/ruby"
+	elif which ruby >/dev/null 2>&1; then
+		ruby_exe="$(which ruby)"
+	else
+		echo 'Could not find Ruby executable' >&2
+		exit 2
+	fi
+fi
+
+"$ruby_exe" --enable-frozen-string-literal -E utf-8 "$(dirname "$0")"/update_completion.rb "$sonic_pi_app_path"
